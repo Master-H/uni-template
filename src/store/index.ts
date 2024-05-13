0ed8a959f01fb92174edc11e46f454954a26eae1
uestchxy <uestchxy@163.com>
2024-05-13 21:55:44 +0800

    Create index.ts

---
+++ src/store/index.ts
@@ -0,0 +1,41 @@
+import {defineStore} from 'pinia';
+import {ref, reactive} from 'vue';
+
+export const useGlobalStore = defineStore('globalStore', () => {
+    const count  = ref<number>(1);
+    const weather  = ref<string>('');
+    interface Info {
+        name: string;
+        age: number;
+        height: number;
+    }
+    const info = reactive<Info>({
+        name: '测试',
+        age: 1,
+        height: 166,
+    });
+
+    const addCount  = () => {
+        count.value = count.value + 1;
+    };
+
+    const addAge = () => {
+        info.age = info.age + 1;
+    };
+
+    const updateWeather =  async (city: string) => {
+        // 模拟接口返回
+        const data = await Promise.resolve(`${city}-天气`);
+        weather.value = data;
+    };
+
+
+    return {
+        count,
+        info,
+        addCount: addCount,
+        addAge,
+        updateWeather,
+        weather,
+    };
+});
