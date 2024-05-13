16b60f9c1812e6f94fd294fb63f114303c31e8d9
uestchxy <uestchxy@163.com>
2024-05-13 21:53:27 +0800

    Create api.ts

---
+++ src/utils/api.ts
@@ -0,0 +1,25 @@
+/**
+ * @file api接口地址管理
+ */
+
+const BASE_URL = 'http://www.xxx.com';
+
+export const API = {
+    // tab1
+    TBA1: {
+        /**
+         * 获取数据
+         */
+        BANNER: `${BASE_URL}/list`,
+    },
+    // tab2接口
+    TAB2: {
+        BANNER: `${BASE_URL}/list`,
+    },
+    // tab3
+    TAB3: {
+        BANNER: `${BASE_URL}/list`,
+    },
+};
+
+export default API;
